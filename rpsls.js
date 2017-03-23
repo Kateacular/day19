026b030ed6199bf2e8263df2b15d30b71a8b7039
Kateacular <kate.orr@detroitlabs.com>
2017-03-23 14:47:32 -0400

    adding instructions and sget line

---
+++ rpsls.js
@@ -0,0 +1,15 @@
+// - Your game should allow the user to select a hand shape and play against the computer 
+// (which will select a random hand shape)
+
+// - The game should notify the user of the outcome, 
+// and should offer the user the option to play again
+
+// - The game should tell the user how they won or lost 
+// (e.g., display the message "Paper disproves Spock!")
+
+// **********
+
+var sget = require("sget");
+
+
+
